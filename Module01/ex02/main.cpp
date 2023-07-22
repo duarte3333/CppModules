116a61e2b3b82dcb2a17b004e29b7fd32e99cd82
duarte_3333 <dsmorais.33@gmail.com>
2023-07-22 14:06:59 +0100

    finished ex02 module01

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+/* Uma referencia nao e uma copia, pois nao ocupa
+memoria adicional, apenas cria um nome alternativo
+para chamar a variavel */
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of string: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
+
+/* Pointer -> str/Ref */
